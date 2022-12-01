7d8bed602daa8c49d8ec4309aaeae83435eb6aca
sdw5588 <sdw5588@rit.edu>
2022-12-01 16:25:53 -0500

    Initial Commit

---
+++ src/Questions.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Questions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!\nWelcome to Advent of Code 2022!");
+    }
+}
